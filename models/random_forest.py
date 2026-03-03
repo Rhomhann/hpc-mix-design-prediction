dccad494ca7cdd99a27ac491ec9bbc0624c177e6
Rhomhann <156308747+Rhomhann@users.noreply.github.com>
2026-03-03 09:26:22 -0500

    Clarify library references in random_forest.py
    Updated comment to clarify library references.

--- models/random_forest.py
+++ models/random_forest.py
@@ -1,6 +1,9 @@
-#Perculair library
+#Perculair libraries-refer to deep_learning.py for more libraries
+
 from sklearn.ensemble import RandomForestRegressor
+
 #Model setup and training
+
 rf_model = RandomForestRegressor(n_estimators=1000, random_state=42)
 rf_model.fit(x_train, y_train)
 rf_predict = rf_model.predict(x_test)
